1e5befe5cd473f79de3a60f016d238ca46364c95
preetsingh10 <preet10syd@gmail.com>
2024-09-02 19:58:16 +1000

    add the eraser

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
 const canvas = document.querySelector(".gridCanvas");
 const gridSizeButton = document.querySelector(".grid-size-btn");
+const eraser = document.querySelector('.eraser');
 function grid(dimensions) {
     let opacity = 0;
     // random color generator
-    function randomRGB(){
-        let red = Math.floor( Math.random()*256);
-        let green = Math.floor( Math.random()*256);
-        let blue = Math.floor( Math.random()*256);
+    function randomRGB() {
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
         return `rgb(${red}, ${green}, ${blue})`;
     }
 
     // column with each div of display flex
-  for (i = 1; i <= dimensions; i++) {
-    const columnDiv = document.createElement("div");
-    columnDiv.setAttribute("style", "display:flex");
-    canvas.appendChild(columnDiv);
+    for (i = 1; i <= dimensions; i++) {
+        const columnDiv = document.createElement("div");
+        columnDiv.setAttribute("style", "display:flex");
+        canvas.appendChild(columnDiv);
 
-    // row of the child elements of column div
-    for (j = 1; j <= dimensions; j++) {
-      const rowSquare = document.createElement("div");
-      rowSquare.classList.add("rowSquare");
+        // row of the child elements of column div
+        for (j = 1; j <= dimensions; j++) {
+            const rowSquare = document.createElement("div");
+            rowSquare.classList.add("rowSquare");
 
-      rowSquare.addEventListener("mouseover", () => {
-     
-        rowSquare.style.backgroundColor = `${randomRGB()}`;
+            rowSquare.addEventListener("mouseover", () => {
 
-      });
-      rowSquare.addEventListener("touchstart", (e) => {
-     
-        rowSquare.style.backgroundColor = `${randomRGB()}`;
-        e.preventDefault();
-      });
+                rowSquare.style.backgroundColor = `${randomRGB()}`;
 
-      columnDiv.appendChild(rowSquare);
+                eraser.addEventListener('click', () => {
+                    rowSquare.style.backgroundColor = 'white';
+                });
+
+            });
+
+
+
+            columnDiv.appendChild(rowSquare);
+        }
     }
-  }
 }
 grid(30);
 gridSizeButton.addEventListener("click", () => {
-  canvas.textContent = "";
+    canvas.textContent = "";
 
-  let userInput = prompt("Select any size upto 100");
-  if (userInput < 100) {
-    const size = userInput;
-    grid(userInput);
-  } else {
-    alert("Please input a number below 100");
-  }
+    let userInput = prompt("Select any size upto 100");
+    if (userInput < 100) {
+        const size = userInput;
+        grid(userInput);
+    } else {
+        alert("Please input a number below 100");
+    }
 });
