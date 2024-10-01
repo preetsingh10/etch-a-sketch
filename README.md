fb3d172b72339cf384bcf13affc9e72e7a023c2f
Preet Singh <preet10syd@gmail.com>
2024-10-02 06:52:59 +1000

    add cell size functionality

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # etch-a-sketch
+This an pixelated sketch canvas.
+
+live preview:https://preetsingh10.github.io/etch-a-sketch/
--- index.js
+++ index.js
@@ -1,110 +1,116 @@
 const canvas = document.querySelector(".gridCanvas");
 const gridSizeButton = document.querySelector(".grid-size-btn");
-const clear = document.querySelector('.clear');
-const eraser = document.querySelector('.eraser');
-const gridRange = document.querySelector('.grid-range');
-const colorPicker = document.querySelector('.colorPicker');
-const randomColor = document.querySelector('.randomColor');
-const randomColors = document.querySelector('.randomColors');
+const clear = document.querySelector(".clear");
+const eraser = document.querySelector(".eraser");
+const gridRange = document.querySelector(".grid-range");
+const colorPicker = document.querySelector(".colorPicker");
+const randomColor = document.querySelector(".randomColor");
+const randomColors = document.querySelector(".randomColors");
+const cellSizeSlider = document.querySelector(".cell-size-range");
 
 // random color generator
 function randomRGB() {
-    let red = Math.floor(Math.random() * 256);
-    let green = Math.floor(Math.random() * 256);
-    let blue = Math.floor(Math.random() * 256);
-    return `rgb(${red}, ${green}, ${blue})`;
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
 }
-function grid(dimensions) {
 
-    let colorOfSketch = 'rgb(0,0,0)';
+// grid maker function
 
-    randomColor.addEventListener('click',()=>{
+function grid(dimensions,cellSize) {
+  let colorOfSketch = "rgb(0,0,0)";
+
+  randomColor.addEventListener("click", () => {
+    colorOfSketch = randomRGB();
+  });
+
+  randomColors.addEventListener("click", () => {
+    const rowSquare = document.querySelectorAll(".rowSquare");
+    rowSquare.forEach((square) => {
+      square.addEventListener("mouseover", () => {
         colorOfSketch = randomRGB();
-    })
+      });
+    });
+  });
 
-    randomColors.addEventListener('click', ()=>{
-        const rowSquare = document.querySelectorAll('.rowSquare');
-        rowSquare.forEach(square => {
-            square.addEventListener('mouseover', ()=>{
-                colorOfSketch = randomRGB();
-            })
-        })
-    })
+  colorPicker.addEventListener("input", () => {
+    colorOfSketch = `${colorPicker.value}`;
+  });
 
-    colorPicker.addEventListener('input', () => {
-        colorOfSketch = `${colorPicker.value}`;
-    })
+  // column with each div of display flex
+  for (i = 1; i <= dimensions; i++) {
+    const columnDiv = document.createElement("div");
+    columnDiv.setAttribute("style", "display:flex");
+    canvas.appendChild(columnDiv);
 
-    // column with each div of display flex
-    for (i = 1; i <= dimensions; i++) {
-        const columnDiv = document.createElement("div");
-        columnDiv.setAttribute("style", "display:flex");
-        canvas.appendChild(columnDiv);
-
-        // row of the child elements of column div
-        for (j = 1; j <= dimensions; j++) {
-            const rowSquare = document.createElement("div");
-            rowSquare.classList.add("rowSquare");
-            rowSquare.addEventListener('mouseover', (e) => {
-                e.preventDefault();
-                rowSquare.style.backgroundColor = `${colorOfSketch}`;
-            })
-            columnDiv.appendChild(rowSquare);
-        }
+    // row of the child elements of column div
+    for (j = 1; j <= dimensions; j++) {
+      const rowSquare = document.createElement("div");
+      rowSquare.classList.add("rowSquare");
+    rowSquare.style.width = `${cellSize}px`
+    rowSquare.style.height = `${cellSize}px`
+      rowSquare.addEventListener("mouseover", (e) => {
+        e.preventDefault();
+        rowSquare.style.backgroundColor = `${colorOfSketch}`;
+      });
+      columnDiv.appendChild(rowSquare);
     }
-    // grid square event listener
-const gridSquares = document.querySelectorAll('.rowSquare');
-// make the hover progressive color fill
-gridSquares.forEach(square => {
+  }
+  // grid square event listener
+  const gridSquares = document.querySelectorAll(".rowSquare");
+  // make the hover progressive color fill
+  gridSquares.forEach((square) => {
     let hover = 0;
-    square.addEventListener('mouseover',()=>{
-    hover+= 0.10;
-    if(hover === 1){
-        square.style.opacity = 1
-        console.log('i am hover')
-    }else{
+    square.addEventListener("mouseover", () => {
+      hover += 0.1;
+      if (hover === 1) {
+        square.style.opacity = 1;
+        console.log("i am hover");
+      } else {
         square.style.opacity = hover;
-        console.log('i am hover')
-    }
-    })
-});
+        console.log("i am hover");
+      }
+    });
+  });
 }
 
 // random colors button event listener
-randomColors.addEventListener('click',()=>{
-
-})
+randomColors.addEventListener("click", () => {});
 
 // clear button listener
-clear.addEventListener('click', () => {
-    const rowSquare = document.querySelectorAll('.rowSquare');
-    rowSquare.forEach(square => {
-        square.style.backgroundColor = 'white';
-        square.style.opacity = '0.10';
-        square.style.hover = '0.10';
-    })
-
+clear.addEventListener("click", () => {
+  const rowSquare = document.querySelectorAll(".rowSquare");
+  rowSquare.forEach((square) => {
+    square.style.backgroundColor = "white";
+    square.style.opacity = "0.10";
+    square.style.hover = "0.10";
+  });
 });
 
-
-
 // slider event listener
-gridRange.addEventListener('input', () => {
+gridRange.addEventListener("input", () => {
+  canvas.textContent = "";
+  grid(gridRange.value,cellSizeSlider.value);
+});
+
+// cell size slider event listener
+cellSizeSlider.addEventListener("input", () => {
     canvas.textContent = "";
-    grid(gridRange.value);
-})
+  grid(gridRange.value,cellSizeSlider.value)
+});
 
 // grid size button listener
 
 gridSizeButton.addEventListener("click", () => {
-    canvas.textContent = "";
+  canvas.textContent = "";
 
-    let userInput = prompt("Select any size upto 100");
-    if (userInput < 100) {
-        grid(userInput);
-    } else {
-        alert("Please input a number below 100");
-    }
+  let userInput = prompt("Select any size upto 100");
+  if (userInput < 100) {
+    grid(userInput);
+  } else {
+    alert("Please input a number below 100");
+  }
 });
 
 // default gird on starting
